01691fb098ef166b85d8776a496ea39f4ed5332a
mngrau97 <mngrau97@gmail.com>
2022-10-21 15:35:20 -0500

    implemented lombok

--- src/main/java/com/devmountain/noteApp/entities/Note.java
+++ src/main/java/com/devmountain/noteApp/entities/Note.java
@@ -18,20 +18,4 @@ public class Note {
 
     @Column(columnDefinition = "text")
     private String body;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
 }
--- src/main/java/com/devmountain/noteApp/entities/User.java
+++ src/main/java/com/devmountain/noteApp/entities/User.java
@@ -1,9 +1,16 @@
 package com.devmountain.noteApp.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,37 +21,4 @@ public class User {
 
     @Column
     private String password;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public User() {
-    }
-
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
 }
